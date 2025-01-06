a0a0a10ca45de90a88e114c22f08f66042a5390d
ArtemVeliulaev <artemveliulaev@gmail.com>
2025-01-06 15:59:38 +0500

    4

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telebot import TeleBot
 import bot_buttons as bt
 from geopy import Photon
 import database as db
-
+import translate as tr
 admins_group = -4661710704
 geolocator = Photon(user_agent="geo_locator", timeout=10)
 
@@ -14,35 +14,6 @@ geolocator = Photon(user_agent="geo_locator", timeout=10)
 # db.add_product("Шефбургер", 41000.00, "Шефбургер — сочная котлета, свежие овощи и пикантные соусы, объединённые в идеальный вкус в мягкой булочке.", 30, "https://kfs-menu.ru/images/menu/shefburger_mini.jpg")
 # db.add_product("Лонгер", 29000.00, "Лонгер — длинная булочка с мягкой текстурой, в которой скрывается сочная начинка, свежие овощи и ароматные соусы.", 30, "https://kfc-burgers.ru/wp-content/uploads/2018/11/longer-kfs.png")
 # db.add_product("Чикенбургер", 25000.00, "Чикенбургер — нежная куриная котлета, свежие овощи и пикантные соусы, собранные в мягкой булочке для идеального вкуса.", 30, "https://kuponoed.ru/wp-content/uploads/2022/10/chikenburger.jpg")
-
-all_text = {"номер_т_кн": {"ru":'📞Поделиться номером телефона📞', "uz":'📞Поделиться номером телефона📞'},
-            "локация_кн": {"ru":'🗺Поделиться локацией🗺',"uz":"🗺Lokatsiyangizni ulashish🗺"},
-            "меню": {"ru":'🍴Меню🍴',"uz":"🍴Menu🍴"},
-            "корзина": {"ru":'Корзина',"uz":"Savatcha"},
-            "отзыв_кн": {"ru":'✍️Отзыв✍️',"uz":"✍️Fikr-mulohaza✍️"},
-            "корзина_кн": {"ru":'🛒Корзина🛒',"uz":"🛒Savatcha🛒"},
-            "назад_кн": {"ru":'Назад',"uz":"Orqaga"},
-            "доб_в_кор": {"ru":'Добавить в корзиину',"uz":"Savatchaga qo'shish"},
-            "очистить_к": {"ru":'Очистить корзину',"uz":"Korzinani tozalash"},
-            "оформить_з": {"ru":'Оформить заказ',"uz":"Buyurtma berish"},
-            "номер_т": {"ru":'Отлично! Теперь поделитесь своим номером телефона',"uz":"Ajoyib! Endi telefon raqamingizni ulashing"},
-            "локация": {"ru":'Отлично! Теперь отправьте локацию',"uz":" Ajoyib! Endi lokatsiyani yuboring"},
-            "ошибка": {"ru":'Ошибка! Отправьте свой номер по кнопке в меню',"uz":"Xato! Telefon raqamingizni menyudagi tugma orqali yuboring"},
-            "успеш_рег": {"ru":'Вы успешно прошли регистрацию.\n\nМеню:',"uz":"Siz muvaffaqiyatli ro'yxatdan o'tdingiz.\n\nMenu:"},
-            "прод_усп": {"ru":'Продукт успешно добавлен в корзину.\n'
-                                  'Желаете что-нибудь еще?:',"uz":"Mahsulot muvaffaqiyatli korzinaga qo'shildi.\n Yana biror narsa xohlaysizmi?"},
-            "выберите_п": {"ru":'Выберите продукт: ',"uz":"Mahsulotni tanlang: "},
-            "моя_к": {"ru":'Ваша корзина:\n',"uz":"Sizning korzinaniz:\n"},
-            "сумма": {"ru":'Итоговая сумма: ',"uz":"Yakuniy summa: "},
-            "кор_уже_очищ": {"ru":'Корзина очищена\n'
-                                  'Хотите что-нибудь заказать?: ',"uz":"Korzina tozalandi\nBiror narsa buyurtma bermoqchimisiz?"},
-            "нов_заказ": {"ru":'Новый заказ от пользователя :',"uz":"Foydalanuvchidan yangi buyurtma :"},
-            "заказ_принят": {"ru":'Ваш заказ принят. Ожидайте',"uz":"Sizning buyurtmangiz qabul qilindi. Kuting."},
-            "цена": {"ru":'Цена:',"uz":"Narx:"},
-            "описание":{"ru":'Описание:',"uz":"Tavsif:"},
-            "отзыв_напишите": {"ru":'Напишите ваш отзыв: ',"uz":"Fikr-mulohazalaringizni yozing: "}}
-
-
 
 users = {}
 bot = TeleBot(token="7334242659:AAFhHWElOHJim9dxUGdAF86nlbd6Ke5mp9k")
@@ -76,8 +47,8 @@ def get_name(message,language):
     user_id = message.from_user.id
     name = message.text
     print(name)
-    bot.send_message(user_id, text = all_text.get("номер_т").get(language),
-                     reply_markup=bt.phone_number_bt(user_id))
+    bot.send_message(user_id, text = tr.all_text.get("номер_т").get(language),
+                     reply_markup=bt.phone_number_bt(language))
     bot.register_next_step_handler(message, get_phone, name,language)
 
 def get_phone(message, name,language):
@@ -86,13 +57,13 @@ def get_phone(message, name,language):
     if message.contact:
         phone_number = message.contact.phone_number
         print(phone_number)
-        bot.send_message(user_id, text= all_text.get("локация").get(language),
-                         reply_markup=bt.location_bt(user_id))
+        bot.send_message(user_id, text= tr.all_text.get("локация").get(language),
+                         reply_markup=bt.location_bt(language))
         bot.register_next_step_handler(message, get_location, name, phone_number,language)
     # если отправил контакт не по кнопке возвращаем в эту же функцию
     else:
-        bot.send_message(user_id, text= all_text.get("ошибка").get(language),
-                         reply_markup=bt.phone_number_bt(user_id))
+        bot.send_message(user_id, text= tr.all_text.get("ошибка").get(language),
+                         reply_markup=bt.phone_number_bt(language))
         bot.register_next_step_handler(message, get_phone, name,language)
 
 def get_location(message, name, phone_number,language):
@@ -101,7 +72,7 @@ def get_location(message, name, phone_number,language):
     address = geolocator.reverse((location.latitude, location.longitude)).address
     print(name, phone_number, address)
     db.add_user(name, phone_number, user_id, language)
-    bot.send_message(user_id, text= all_text.get("успеш_рег").get(language), reply_markup=bt.main_menu_bt(user_id))
+    bot.send_message(user_id, text= tr.all_text.get("успеш_рег").get(language), reply_markup=bt.main_menu_bt(user_id))
 
 @bot.callback_query_handler(lambda call: call.data in ["back", "cart", "plus", "minus",
                                                        "to_cart", "back_product", "order", "clear_cart"])
@@ -109,7 +80,7 @@ def calls(call):
     user_id = call.message.chat.id
     if call.data == "back":
         language = db.find_language_db(user_id)[0]
-        bot.send_message(user_id, text=all_text.get("меню").get(language), reply_markup=bt.main_menu_bt(user_id))
+        bot.send_message(user_id, text=tr.all_text.get("меню").get(language), reply_markup=bt.main_menu_bt(user_id))
     elif call.data == "plus":
         current_amount = users.get(user_id).get("pr_count")
         users[user_id]["pr_count"] += 1
@@ -127,47 +98,47 @@ def calls(call):
         bot.delete_message(user_id, call.message.message_id)
         all_products = db.get_pr_id_name()
         language = db.find_language_db(user_id)[0]
-        bot.send_message(user_id, text=all_text.get("прод_усп").get(language), reply_markup=bt.product_in(all_products,user_id))
+        bot.send_message(user_id, text=tr.all_text.get("прод_усп").get(language), reply_markup=bt.product_in(all_products,user_id))
     elif call.data=="back_product":
         bot.delete_message(user_id, call.message.message_id)
         all_products = db.get_pr_id_name()
         language = db.find_language_db(user_id)[0]
-        bot.send_message(user_id, text=all_text.get("выберите_п").get(language), reply_markup=bt.product_in(all_products,user_id))
+        bot.send_message(user_id, text=tr.all_text.get("выберите_п").get(language), reply_markup=bt.product_in(all_products,user_id))
     elif call.data == "cart":
         cart = db.get_user_cart(user_id)
         print(cart)
         language = db.find_language_db(user_id)[0]
-        full_text =all_text.get("моя_к").get(language)
+        full_text =tr.all_text.get("моя_к").get(language)
         count =0
         total_price = 0
         for product in cart:
             count +=1
             full_text += f"{count}.{product[0]} x {product[1]} = {product[2]}\n"
             total_price += product[2]
-        full_text += f"{all_text.get("сумма").get(language)} {total_price} sum"
+        full_text += f"{tr.all_text.get("сумма").get(language)} {total_price} sum"
         cart_info = db.get_cart_id_name(user_id)
         bot.send_message(user_id, full_text, reply_markup = bt.cart_in(cart_info,user_id))
     elif call.data == "clear_cart":
         bot.delete_message(user_id, call.message.message_id)
         all_products = db.get_pr_id_name()
         language = db.find_language_db(user_id)[0]
-        bot.send_message(user_id, text=all_text.get("кор_уже_очищ").get(language), reply_markup=bt.product_in(all_products,user_id))
+        bot.send_message(user_id, text=tr.all_text.get("кор_уже_очищ").get(language), reply_markup=bt.product_in(all_products,user_id))
     elif call.data == "order":
         cart = db.get_user_cart(user_id)
         print(cart)
         language = db.find_language_db(user_id)[0]
-        full_text = f"{all_text.get("нов_заказ").get(language)}{user_id}:\n"
+        full_text = f"{"Новый заказ от пользователя "}{user_id}:\n"
         count = 0
         total_price = 0
         for product in cart:
             count += 1
             full_text += f"{count}.{product[0]} x {product[1]} = {product[2]}\n"
             total_price += product[2]
-        full_text += f"{all_text.get("сумма").get(language)} {total_price} sum"
+        full_text += f"{"Сумма:"} {total_price} sum"
         bot.delete_message(user_id, call.message.message_id)
         db.delete_user_cart(user_id)
         bot.send_message(admins_group, full_text)
-        bot.send_message(user_id, text= all_text.get("заказ_принят").get(language), reply_markup = bt.main_menu_bt(user_id))
+        bot.send_message(user_id, text= tr.all_text.get("заказ_принят").get(language), reply_markup = bt.main_menu_bt(user_id))
 
 @bot.callback_query_handler(lambda call: "prod_" in call.data)
 def get_prod_info(call):
@@ -180,8 +151,8 @@ def get_prod_info(call):
     users[user_id] = {"pr_id": product_id, "pr_name": product_info[0],
                       "pr_count": 1, "pr_price": product_info[1]}
     bot.send_photo(chat_id=user_id, photo=product_info[3], caption=f"{product_info[0]}\n"
-                                                                   f"{all_text.get("цена").get(language)} {product_info[1]} sum\n"
-                                                                   f"{all_text.get("описание").get(language)} {product_info[2]}",
+                                                                   f"{tr.all_text.get("цена").get(language)} {product_info[1]} sum\n"
+                                                                   f"{tr.all_text.get("описание").get(language)} {product_info[2]}",
                    reply_markup=bt.plus_or_minus_in(user_id))
 
 @bot.callback_query_handler(lambda call: "delete_" in call.data)
@@ -192,14 +163,14 @@ def delete_prod_cart(call):
     cart = db.get_user_cart(user_id)
     print(cart)
     language = db.find_language_db(user_id)[0]
-    full_text = all_text.get("моя_к").get(language)
+    full_text = tr.all_text.get("моя_к").get(language)
     count = 0
     total_price = 0
     for product in cart:
         count += 1
         full_text += f"{count}.{product[0]} x {product[1]} = {product[2]}\n"
         total_price += product[2]
-    full_text += f"{all_text.get("сумма").get(language)} {total_price} sum"
+    full_text += f"{tr.all_text.get("сумма").get(language)} {total_price} sum"
     cart_info = db.get_cart_id_name(user_id)
     bot.edit_message_text(chat_id= user_id, message_id = call.message.message_id,
                           text= full_text, reply_markup = bt.cart_in(cart_info,user_id))
@@ -211,11 +182,11 @@ def text_handler(message):
     language = db.find_language_db(user_id)[0]
     if message.text == "🍴Меню🍴" or message.text == "🍴Menu🍴":
         all_products = db.get_pr_id_name()
-        bot.send_message(user_id, text=all_text.get("выберите_п").get(language), reply_markup=bt.product_in(all_products,user_id))
+        bot.send_message(user_id, text=tr.all_text.get("выберите_п").get(language), reply_markup=bt.product_in(all_products,user_id))
     elif message.text == "✍️Отзыв✍️" or message.text == "✍️Fikr-mulohaza✍️":
-        bot.send_message(user_id, text= all_text.get("отзыв_напишите").get(language))
+        bot.send_message(user_id, text= tr.all_text.get("отзыв_напишите").get(language))
     elif message.text == "🛒Корзина🛒" or message.text == "🛒Savatcha🛒":
-        bot.send_message(user_id, text =all_text.get("моя_к").get(language))
+        bot.send_message(user_id, text =tr.all_text.get("моя_к").get(language))
 
 # поддержание запуска бота
 bot.infinity_polling()
--- bot_buttons.py
+++ bot_buttons.py
@@ -1,21 +1,19 @@
 from itertools import product
 from telebot import TeleBot
 import database as db
-import bot
+import translate as tr
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-def phone_number_bt(user_id):
+def phone_number_bt(language):
     kb = ReplyKeyboardMarkup(resize_keyboard= True)
-    language = db.find_language_db(user_id)[0]
-    button = KeyboardButton(text = bot.all_text.get("номер_т_кн").get(language),
+    button = KeyboardButton(text = tr.all_text.get("номер_т_кн").get(language),
                             request_contact = True)
     kb.add(button)
     return kb
 
-def location_bt(user_id):
-    language = db.find_language_db(user_id)[0]
+def location_bt(language):
     kb = ReplyKeyboardMarkup(resize_keyboard= True)
-    button = KeyboardButton(text = bot.all_text.get("локация_кн").get(language),
+    button = KeyboardButton(text = tr.all_text.get("локация_кн").get(language),
                             request_location = True)
     kb.add(button)
     return kb
@@ -30,9 +28,9 @@ def languages_in():
 def main_menu_bt(user_id):
     language = db.find_language_db(user_id)[0]
     kb = ReplyKeyboardMarkup(resize_keyboard= True)
-    button1 = KeyboardButton(text=bot.all_text.get("меню").get(language))
-    button2 = KeyboardButton(text=bot.all_text.get("отзыв_кн").get(language))
-    button3 = KeyboardButton(text=bot.all_text.get("корзина_кн").get(language))
+    button1 = KeyboardButton(text=tr.all_text.get("меню").get(language))
+    button2 = KeyboardButton(text=tr.all_text.get("отзыв_кн").get(language))
+    button3 = KeyboardButton(text=tr.all_text.get("корзина_кн").get(language))
     kb.row(button1)
     kb.row(button2,button3)
     return kb
@@ -41,8 +39,8 @@ def product_in(all_products,user_id):
     kb = InlineKeyboardMarkup(row_width =2)
     #статичные
     language = db.find_language_db(user_id)[0]
-    back = InlineKeyboardButton(text = bot.all_text.get("назад_кн").get(language), callback_data= "back")
-    cart = InlineKeyboardButton(text = bot.all_text.get("корзина").get(language), callback_data= "cart")
+    back = InlineKeyboardButton(text = tr.all_text.get("назад_кн").get(language), callback_data= "back")
+    cart = InlineKeyboardButton(text = tr.all_text.get("корзина").get(language), callback_data= "cart")
     #Динамичные
     buttons = [InlineKeyboardButton(text= f"{product[1]}", callback_data =f"prod_{product[0]}")
                for product in all_products]
@@ -54,8 +52,8 @@ def product_in(all_products,user_id):
 def plus_or_minus_in(user_id, plus_or_minus="", current_amount = 1):
     kb = InlineKeyboardMarkup(row_width=3)
     language = db.find_language_db(user_id)[0]
-    back = InlineKeyboardButton(text = bot.all_text.get("назад_кн").get(language), callback_data = "back_product")
-    to_cart = InlineKeyboardButton( text = bot.all_text.get("доб_в_кор").get(language), callback_data = "to_cart")
+    back = InlineKeyboardButton(text = tr.all_text.get("назад_кн").get(language), callback_data = "back_product")
+    to_cart = InlineKeyboardButton( text = tr.all_text.get("доб_в_кор").get(language), callback_data = "to_cart")
     minus = InlineKeyboardButton( text = "-", callback_data = "minus")
     plus = InlineKeyboardButton(text = "+", callback_data = "plus")
     count = InlineKeyboardButton(text = f"{current_amount}", callback_data = "none")
@@ -72,9 +70,9 @@ def plus_or_minus_in(user_id, plus_or_minus="", current_amount = 1):
 def cart_in(cart,user_id):
     kb = InlineKeyboardMarkup(row_width=1)
     language = db.find_language_db(user_id)[0]
-    clear = InlineKeyboardButton(text =bot.all_text.get("очистить_к").get(language) , callback_data = "clear_cart")
-    order = InlineKeyboardButton(text=bot.all_text.get("оформить_з").get(language), callback_data="order")
-    back = InlineKeyboardButton(text=bot.all_text.get("назад_кн").get(language), callback_data="back_product")
+    clear = InlineKeyboardButton(text =tr.all_text.get("очистить_к").get(language) , callback_data = "clear_cart")
+    order = InlineKeyboardButton(text=tr.all_text.get("оформить_з").get(language), callback_data="order")
+    back = InlineKeyboardButton(text=tr.all_text.get("назад_кн").get(language), callback_data="back_product")
     kb.add(order, clear, back)
     if cart:
         products = [InlineKeyboardButton(text = f"❌ {product[1]}", callback_data = f"delete_{product[0]}") for product in cart]
